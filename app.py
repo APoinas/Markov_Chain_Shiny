43d97ef84b7179fcf54fab669ee3c40ba1d43082
Arnaud Poinas <noszeros@gmail.com>
2025-02-06 16:43:15 +0100

    First commit

---
+++ app.py
@@ -0,0 +1,234 @@
+import os as os
+
+abspath = os.path.abspath(__file__)
+dname = os.path.dirname(abspath)
+os.chdir(dname)
+
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+from pathlib import Path
+from shiny import App, Inputs, Outputs, Session, render, ui, reactive
+from utils import Snake_Ladder_Matrix, Make_board_SL, Vignette_Matrix, Monopoly_Matrix, Clear_patches, Make_board_M, Get_Stationnary
+
+app_ui = ui.page_fluid(
+    ui.navset_pill(
+        ui.nav_panel("Echelles et serpents",
+            ui.h1("Simulation du jeu des échelles et des serpents", align = "center"),
+            ui.layout_columns(
+                ui.output_image("Image_SL"),
+                ui.output_plot("Probability_Board", height="550px"),
+                col_widths=(-1, 5, 6),
+                height="450px"
+            ),
+            ui.layout_columns(
+                ui.card(
+                    ui.layout_columns(
+                        ui.input_action_button("Decrease_move_nb", "-"),
+                        ui.div(
+                            ui.output_ui("Display_move_nb"),
+                            align="center"
+                        ),
+                        ui.input_action_button("Increase_move_nb", "+"),
+                        col_widths=(3, 6, 3),
+                        align="center"
+                    )
+                ),
+                col_widths=(-4, 4),
+                align="center"
+            ),
+        ),
+        ui.nav_panel("Collectionneur de vignettes",
+            ui.h1("Simulation du problème du collectionneur de vignettes", align="center"),
+            ui.layout_columns(
+                ui.card(ui.input_slider("Move_nb_V", "Nombre total de vignettes:", min=1, max=15, value=10), align="center"),
+                col_widths=(-5, 2, 5),
+                align="center",
+            ),
+            ui.div(ui.output_plot("Probability_Vignette", width="1000px", height="200px"), align="center"),
+            ui.layout_columns(
+                ui.card(
+                    ui.layout_columns(
+                        ui.input_action_button("Decrease_move_nb_V", "-"),
+                        ui.div(
+                            ui.output_ui("Display_move_nb_V"),
+                            align="center"
+                        ),
+                        ui.input_action_button("Increase_move_nb_V", "+"),
+                        col_widths=(3, 6, 3),
+                        align="center",
+                    )
+                ),
+                col_widths=(-4, 4),
+                align="center"
+            ),
+        ),
+        ui.nav_panel("Monopoly",
+            ui.h1("Simulation du jeu de Monopoly", align = "center"),
+            ui.layout_columns(
+                ui.output_image("Image_M"),
+                ui.output_plot("Probability_Monopoly", width="900px"),
+                align="center",
+                col_widths=(-1, 4, 3),
+                height="600px"
+            ),
+            ui.layout_columns(
+                ui.card(
+                    ui.layout_columns(
+                        ui.input_action_button("Decrease_move_nb_M", "-"),
+                        ui.div(
+                            ui.output_ui("Display_move_nb_M"),
+                            align="center"
+                        ),
+                        ui.input_action_button("Increase_move_nb_M", "+"),
+                        col_widths=(3, 6, 3),
+                        align="center",
+                    )
+                ),
+                col_widths=(-4, 4),
+                align="center"
+            ),
+            ui.layout_columns(ui.card(ui.input_action_button("Infinity_M", "Infinity"), align="center"), col_widths=(-5, 2))
+        )
+    )
+)
+
+def server(input: Inputs, output: Outputs, session: Session):
+    
+    Move_nb = reactive.value(1)
+    Move_nb_M = reactive.value(1)
+    Move_nb_V = reactive.value(2)
+    Infinity_M = reactive.value(False)
+    
+    @reactive.effect
+    @reactive.event(input.Increase_move_nb)
+    def Increase_move_nb():
+        Move_nb.set(Move_nb() + 1)
+        
+    @reactive.effect
+    @reactive.event(input.Decrease_move_nb)
+    def Decrease_move_nb():
+        if Move_nb() >= 1:
+            Move_nb.set(Move_nb() - 1)
+            
+    @reactive.effect
+    @reactive.event(input.Increase_move_nb_M)
+    def Increase_move_nb():
+        Move_nb_M.set(Move_nb_M() + 1)
+        
+    @reactive.effect
+    @reactive.event(input.Decrease_move_nb_M)
+    def Decrease_move_nb():
+        if Move_nb_M() >= 1:
+            Move_nb_M.set(Move_nb_M() - 1)
+            
+    @reactive.effect
+    @reactive.event(input.Increase_move_nb_V)
+    def Increase_move_nb():
+        Move_nb_V.set(Move_nb_V() + 1)
+        
+    @reactive.effect
+    @reactive.event(input.Decrease_move_nb_V)
+    def Decrease_move_nb():
+        if Move_nb_V() >= 1:
+            Move_nb_V.set(Move_nb_V() - 1)
+    
+    @reactive.effect
+    @reactive.event(input.Infinity_M)
+    def Switch_Infinity_M():
+        Infinity_M.set(not Infinity_M())
+    
+#    @render.text
+#    def Test_text():
+#        return f"Value: {Test_nb()}"
+
+    @render.ui
+    def Display_move_nb():
+        return ui.HTML(f"Nombre de lancers de dés:<br> {Move_nb()}")
+    
+    @render.ui
+    def Display_move_nb_M():
+        return ui.HTML(f"Nombre de lancers de dés:<br> {Move_nb_M()}")
+    
+    @render.ui
+    def Display_move_nb_V():
+        return ui.HTML(f"Nombre de vignettes achetées:<br> {Move_nb_V()}")
+
+    @render.plot
+    def Probability_Board():
+        move = Move_nb()
+        v = np.zeros((1, 100))
+        v[0, 0] = 1
+        P = Snake_Ladder_Matrix()
+        M = 100 * Make_board_SL(v.dot(np.linalg.matrix_power(P, move)))
+        
+        annot = np.zeros((10, 10))
+        annot = np.array(["e" for _ in range(100)])
+        annot = np.round(M, 2)
+        annot = annot.astype("U")
+        annot[annot == "0.0"] = "<0.01"
+        annot = np.char.add(annot, "%")
+        
+        sns.set_style(style='white')
+        ax = sns.heatmap(M, annot=annot, cbar=False, linewidth=.5, linecolor="black", fmt="",
+                    xticklabels=False, yticklabels=False, mask=M==0, cmap=sns.color_palette("crest", as_cmap=True))
+        ax.set_xlim(0, 10.1)
+        ax.set_ylim(10.1, 0)
+        ax.set_aspect(0.7)
+    
+    @render.plot
+    def Probability_Vignette():
+        move = Move_nb_V()
+        n = input.Move_nb_V()
+        v = np.zeros((1, n+1))
+        v[0, 0] = 1
+        P = Vignette_Matrix(n)
+        p = 100 * v.dot(np.linalg.matrix_power(P, move))
+        
+        annot = np.round(p, 2).astype("U")
+        annot[annot == "0.0"] = "<0.01"
+        annot = np.char.add(annot, "%")
+        
+        sns.set_style(style='white')
+        ax = sns.heatmap(p, annot=annot, cbar=False, linewidth=.5, linecolor="black", fmt="",
+                    xticklabels=False, yticklabels=False, mask=p==0, cmap=sns.color_palette("crest", as_cmap=True))
+        ax.set_xlim(0, n + 1.1)
+        ax.set_ylim(-1, 1.1)
+        for i in range(n+1):
+            ax.text(0.4 + i, -0.3, str(i))
+        ax.set_aspect(0.7)
+    
+    @render.image
+    def Image_SL():
+        img = {"src": Path(dname + "/www") / "snakesandladders.png", "width": "600px"}  
+        return img
+    
+    @render.plot
+    def Probability_Monopoly():
+        move = Move_nb_M()
+        P = Monopoly_Matrix()
+        if Infinity_M():
+            M = 100 * Get_Stationnary(P)
+        else:
+            v = np.zeros((1, 123))
+            v[0, 0] = 1
+            M = 100 * v.dot(np.linalg.matrix_power(P, move))
+            M = M.flatten()
+        M2 = M[:40] + M[40:80] + M[80:120]
+        M2 = np.append(M2, np.sum(M[120:]))
+
+        cmap = sns.color_palette("crest", n_colors=40)
+        try:
+            Clear_patches(ax)
+        except:
+            pass
+        fig, ax = Make_board_M(M2, cmap, debug=False)
+    
+    @render.image
+    def Image_M():
+        img = {"src": Path(dname + "/www") / "Monopoly.jpg", "width": "600px"}  
+        return img
+
+app = App(app_ui, server)
+
---
+++ utils.py
@@ -0,0 +1,192 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.patches import Rectangle
+
+#import matplotlib.pyplot as plt
+#import seaborn as sns
+
+############################ Collectionneur Coupons ########################################
+
+def Vignette_Matrix(n):
+    P = np.zeros((n+1, n+1))
+    np.fill_diagonal(P, np.arange(0, n+1)/n)
+    np.fill_diagonal(P[:-1, 1:], 1-np.arange(0, n)/n)
+    return P
+
+############################ Snake and ladders ########################################
+
+def Snake_Ladder_Matrix():
+    P = np.zeros((100, 100))
+    for i in range(94):
+        P[i, (i+1):(i+7)] = 1/6
+    for i in range(94, 100):
+        P[i, i] = (i-93)/6
+        P[i, (i+1):(i+7)] = 1/6
+    for (i, j) in zip([2, 7, 27, 57, 74, 79, 89, 16, 51, 56, 61, 87, 94, 96], [20, 29, 83, 76, 85, 99, 90, 12, 28, 39, 21, 17, 50, 78]):
+        for k in range(100):
+            if P[k, i] != 0:
+                P[k, j] = P[k, i]
+                P[k, i] = 0
+    return P
+
+def Make_board_SL(v):
+    M = np.zeros((10, 10))
+    for i in range(10):
+        u = v[0, (100-10*(i+1)):(100-10*i)]
+        if i % 2 == 0:
+            u = np.flip(u)
+        M[i, :] = u.flatten()
+    return M
+
+############################ Monopoly ########################################
+
+def Monopoly_Matrix():
+    board = np.zeros((40, 3, 40, 3))
+    prison = np.zeros((3, 3))
+    board_to_prison = np.zeros((40, 3, 3))
+    prison_to_board = np.zeros((3, 40, 3))
+
+    nb_comb = [i for i in range(1, 7)] + [6 - i for i in range(1, 6)] # [1, 2, 3, 4, 5, 6, 5, 4, 3, 2, 1]
+    #nb_comb_not_double = [nb_comb[i] if i % 2 == 1 else nb_comb[i] - 1 for i in range(11)] # [0, 2, 2, 4, 4, 6, 4, 4, 2, 2, 0]
+    #nb_comb_double = [1 if i % 2 == 0 else 0 for i in range(11)] # [1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1]
+
+    def Create_Proba_Matrix(board, prison, board_to_prison, prison_to_board):
+        P = np.zeros((123, 123))
+        P[:40, :40] = board[:, 0, :, 0]
+        P[:40, 40:80] = board[:, 0, :, 1]
+        P[:40, 80:120] = board[:, 0, :, 2]
+        P[:40, 120:] = board_to_prison[:, 0, :]
+        P[40:80, :40] = board[:, 1, :, 0]
+        P[40:80, 40:80] = board[:, 1, :, 1]
+        P[40:80, 80:120] = board[:, 1, :, 2]
+        P[40:80, 120:] = board_to_prison[:, 1, :]
+        P[80:120, :40] = board[:, 2, :, 0]
+        P[80:120, 40:80] = board[:, 2, :, 1]
+        P[80:120, 80:120] = board[:, 2, :, 2]
+        P[80:120, 120:] = board_to_prison[:, 2, :]
+        P[120:, :40] = prison_to_board[:, :, 0]
+        P[120:, 40:80] = prison_to_board[:, :, 1]
+        P[120:, 80:120] = prison_to_board[:, :, 2]
+        P[120:, 120:] = prison
+        return P
+
+    ### Pure dice proba
+    x = np.arange(40)
+
+    # Dealing with doubles
+    for move in [2, 4, 6, 8, 10, 12]:
+        board[x, 0, np.roll(x, -move), 1] = 1/36
+        board[x, 1, np.roll(x, -move), 2] = 1/36
+    board_to_prison[x, 2, 0] = 1/6
+    move_list = [i for i in range(3, 12)]
+    nb_comb_list = [2, 2, 4, 4, 6, 4, 4, 2, 2]
+
+    # Dealing with non doubles
+    for move, nb in zip(move_list, nb_comb_list):
+        board[x, 0, np.roll(x, -move), 0] = nb/36
+        board[x, 1, np.roll(x, -move), 0] = nb/36
+        board[x, 2, np.roll(x, -move), 0] = nb/36
+        
+    # Dealing with the prison
+    prison[0, 1] = 30/36
+    prison[1, 2] = 30/36
+    prison_to_board[0, [11, 13, 15, 17, 19, 21], 0] = 1/36
+    prison_to_board[1, [11, 13, 15, 17, 19, 21], 0] = 1/36
+    prison_to_board[2, 11:22, 0] = np.array(nb_comb)/36
+
+    ### Special squares
+    # Go to jail
+    gtj_sq = np.sum(board[:, :, 30, :], axis=2)
+    board_to_prison[:, :, 0] += gtj_sq
+    board[:, :, 30, :] = 0
+
+    ## Comunity chest (16 cards)
+    cc_sq = [2, 17, 33]
+    for sq in cc_sq:
+        proba_sq = np.copy(board[:, :, sq, :])
+        board[:, :, sq, :] = proba_sq * 14 / 16 # Not moving
+        board[:, :, 0, :] += proba_sq * 1 / 16 # Advance to Go [0]
+        board_to_prison[:, :, 0] += np.sum(proba_sq, axis=2) * 1 / 16 # Go to Jail [40]
+
+    # Chance (16 cards)
+    chance_sq = [7, 22, 36]
+    nearest_rr = [15, 25, 5]
+    nearest_ut = [12, 28, 12]
+    for sq, rr, ut in zip(chance_sq, nearest_rr, nearest_ut):
+        proba_sq = np.copy(board[:, :, sq, :])
+        board[:, :, sq, :] = proba_sq * 7 / 16 # Not moving
+        board[:, :, 0, :] += proba_sq * 1 / 16 # Advance to Go [0]
+        board[:, :, 5, :] += proba_sq * 1 / 16 # Take a trip to Reading Railroad [5]
+        board[:, :, 11, :] += proba_sq * 1 / 16 # Advance to St. Charles Place [11]
+        board[:, :, 24, :] += proba_sq * 1 / 16 # Advance to Illinois Avenue [24]
+        board[:, :, 39, :] += proba_sq * 1 / 16 # Advance to Boardwalk [39]
+        board[:, :, sq - 3, :] += proba_sq * 1 / 16 # Go Back 3 Spaces [4, 19, 33]
+        board[:, :, rr, :] += proba_sq * 1 / 16 # Advance to the nearest Railroad [15, 25, 5]
+        board[:, :, ut, :] += proba_sq * 1 / 16 # Advance token to nearest Utility [12, 28, 12]
+        board_to_prison[:, :, 0] += np.sum(proba_sq, axis=2) * 1 / 16 # Go to Jail [40]
+
+    return Create_Proba_Matrix(board, prison, board_to_prison, prison_to_board)
+
+def Clear_patches(ax):
+    for p in ax.patches:
+        p.set_visible(False)
+        p.remove()
+
+def Make_board_M(v, cmap, debug=False):
+    # Setting up colors
+    n_col = len(cmap)
+    v_clamp = (v - min(v))/max(v)
+    id_col = np.floor(v_clamp * n_col)
+    id_col[id_col == n_col] = n_col-1
+    id_col = id_col.astype(int)
+    col_sq = np.array(cmap)[id_col]
+    col_sq[v == 0] = 1
+    
+    # Setting up labels
+    lab = np.round(v, 2).astype(str)
+    lab[lab == "0.0"] = "<0.01"
+    lab = np.char.add(lab, "%")
+    lab[v == 0] = ""
+    lab_col = ["white" if p >= 0.5 else "black" for p in v_clamp]
+    
+    fig, ax = plt.subplots()
+    shift1 = 0.85
+    shift2 = 0.35
+    ax.set_xlim([-0.1, 15.1])
+    ax.set_ylim([-0.1, 15.1])
+    ax.set_axis_off()
+    for coord, id in zip([(0, 0), (12, 0), (0, 12), (12, 12)], [0, 30, 10, 20]):
+        ax.add_patch(Rectangle(coord, 3, 3, fill=True, edgecolor="black", facecolor=col_sq[id]))
+        if coord == (0, 12):
+            ax.text(coord[0] + 1, 14.3, lab[id], color=lab_col[id])
+        else:
+            ax.text(coord[0] + 1, coord[1] + 1.3, lab[id], color=lab_col[id])
+        if debug:
+            ax.text(*coord, str(id))
+    for i in range(3, 12):
+        ax.add_patch(Rectangle((i, 0), 1, 3, fill=True, edgecolor="black", facecolor=col_sq[42 - i]))
+        ax.add_patch(Rectangle((i, 12), 1, 3, fill=True, edgecolor="black", facecolor=col_sq[i + 8]))
+        ax.text(i + shift2, shift1, lab[42 - i], rotation=90, color=lab_col[42 - i])
+        ax.text(i + shift2, 12 + shift1, lab[i + 8], rotation=90, color=lab_col[i + 8])
+        if debug:
+            ax.text(i, 0, str(42 - i))
+            ax.text(i, 12, str(i + 8))
+    for j in range(3, 12):
+        ax.add_patch(Rectangle((0, j), 3, 1, fill=True, edgecolor="black", facecolor=col_sq[j - 2]))
+        ax.add_patch(Rectangle((12, j), 3, 1, fill=True, edgecolor="black", facecolor=col_sq[32 - j]))
+        ax.text(shift1, j + shift2, lab[j - 2], color=lab_col[j - 2])
+        ax.text(12 + shift1, j + shift2, lab[32 - j], color=lab_col[32 - j])
+        if debug:
+            ax.text(0, j, str(j - 2))
+            ax.text(12, j, str(32 - j))
+    ax.add_patch(Rectangle((1, 12), 2, 2, fill=True, edgecolor="black", facecolor=col_sq[40]))
+    ax.text(1.5, 12.8, lab[40], color=lab_col[40])
+    if debug:
+            ax.text(1, 12, "40")
+    ax.set_aspect("equal")
+    return fig, ax
+
+def Get_Stationnary(P):
+    _, eigval = np.linalg.eig(P.T)
+    v = eigval[:, 0]
+    return abs(v / sum(v))
